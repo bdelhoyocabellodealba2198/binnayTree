45b929900d22239cee6289a4a00ce06d448ec7f9
Beatriz <blackmirror2198@gmail.com>
2018-01-11 01:48:08 +0100

    done

--- binnayTree/main.cpp
+++ binnayTree/main.cpp
@@ -15,7 +15,9 @@ void menu();
 Nodo *crearNodo(int);
 void insertNode(Nodo *&arbol, int n);
 Nodo *arbol = NULL;
-
+bool busqueda(Nodo *arbol, int n);
+void preOrder(Nodo *arbol);
+void inOrder(Nodo *arbol);
 int main()
 {
 
@@ -51,7 +53,11 @@ void menu()
 		std::cout << "MENU" << std::endl;
 		std::cout << "1.Insertar un nuevo nodo" << std::endl;
 		std::cout << "2.Imprimir arbol" << std::endl;
-		std::cout << "3.Salir" << std::endl;
+		std::cout << "3.Busca un elemento" << std::endl;
+		std::cout << "4.Recorrer el arbol en peOrder" << std::endl;
+		std::cout << "5.Recorrer el arbol en inOrder" << std::endl;
+		std::cout << "6.Salir" << std::endl;
+
 		std::cin >> option;
 		switch (option)
 		{
@@ -65,11 +71,36 @@ void menu()
 			imprimirArbol(arbol, counter);
 			system("pause");
 			break;
+		case 3:
+			std::cout << std::endl;
+			std::cout << "Que elemento quiere buscar : ";
+			cin >> dato;
+
+			if(busqueda(arbol, dato) == true)
+			{
+				std::cout << "El elemento " << dato << " " << "ha sido encontrado en el arbol" << std::endl;
+
+			}else
+			{
+				std::cout << "El elemento " << dato << " " << " NO	ha sido encontrado en el arbol" << std::endl;
+
+			}
+			system("pause");
+			break;
+		case 4:
+			std::cout << "recorremos el arbol en preOrder.." << std::endl;
+			preOrder(arbol);
+			system("pause");
+			break;
+		case 5:
+			inOrder(arbol);
+			system("pause");
+			break;
 		default:
 			break;
 		}
 		system("cls");
-	} while (option != 3);
+	} while (option != 6);
 }
 //función para crear un nuevo nodo
 Nodo * crearNodo(int n)
@@ -100,5 +131,46 @@ void insertNode(Nodo *&arbol, int n)
 			insertNode(arbol->der, n);
 		}
 		
+	}
+}
+
+bool busqueda(Nodo * arbol, int n)
+{
+	if(arbol == NULL) //si el arbol esta vacío retornamos falsle
+	{
+		return false;
+	}
+	else if(arbol->dato = n)
+	{
+		return true;
+	}
+	else if(n< arbol->dato) //si el elemento es mas peque buscamos por el lado izquierdo
+	{
+		busqueda(arbol->izq, n);
+	}
+	else if (n> arbol->dato) //si el elemento es mas grande buscamos po el lao derecho
+	{
+		busqueda(arbol->der, n);
+	}
+}
+
+void preOrder(Nodo * arbol)
+{
+	if (arbol == NULL) { return; }
+	else {
+		std::cout << arbol->dato << std::endl;
+		preOrder(arbol->izq);
+		preOrder(arbol->der);
+	}
+}
+
+void inOrder(Nodo * arbol)
+{
+	if (arbol == NULL) { return; }
+	else {
+		inOrder(arbol->izq);
+		std::cout << arbol->dato << "-";
+		inOrder(arbol->der);
+
 	}
 }
